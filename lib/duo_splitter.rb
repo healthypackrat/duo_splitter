c6294feb8d86bb1bc9eb2b29546fbea2774b6221
healthypackrat <healthypackrat@gmail.com>
2019-01-24 16:48:06 +0900

    Initial commit
    $ bundle gem duo_splitter --exe --no-coc --no-ext --mit --test=rspec

---
+++ lib/duo_splitter.rb
@@ -0,0 +1,6 @@
+require "duo_splitter/version"
+
+module DuoSplitter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/duo_splitter/version.rb
@@ -0,0 +1,3 @@
+module DuoSplitter
+  VERSION = "0.1.0"
+end
